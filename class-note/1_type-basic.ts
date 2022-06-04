37339c81fc3e02599ee28df7fb63cf597e090b86
dev_sm <tmdals9974@naver.com>
2022-06-05 02:24:39 +0900

    2일차 공부

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,29 @@
+// JS 문자열 선언
+// var str = "hello";
+
+// TS 선언
+let str: string = "hello";
+let num: number = 10;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number] = ["gangnam", 1];
+let address2: [string, number][] = [["gangnam", 1]];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//   name: 'capt',
+//   age: 100,
+// }
+let person: { name: string; age: number } = {
+  age: 20,
+  name: "capt",
+};
+
+// TS 진위값
+let show: boolean = true;
---
+++ class-note/function.js
@@ -0,0 +1,5 @@
+function _sum(a, b) {
+  return a + b;
+}
+
+_sum(1, 2);
